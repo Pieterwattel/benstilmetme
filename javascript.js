f585998e29a75dfe4cf61914baf6f42223ac3fd0
Senorpedrito <pieterwattel@gmail.com>
2024-06-12 20:10:59 +0200

    add a scroll down text, format the button

--- javascript.js
+++ javascript.js
@@ -6,8 +6,22 @@ window.addEventListener('scroll', () => {
 
     // Adjust the fade fraction to be twice as fast
     const fadeFraction = (scrollTop - fadeStart) / ((fadeEnd - fadeStart));
-    const videoOpacity = Math.min(Math.max(0.2 - fadeFraction, 0), 1);
+    const videoOpacity = Math.min(Math.max(0.05 - fadeFraction, 0), 1);
     
     const video = document.querySelector('.header video');
+    video.style.opacity = videoOpacity;
+});
+
+window.addEventListener('scroll', () => {
+    const scrollTop = window.scrollY;
+    const headerHeight = document.querySelector('.scrolltext').offsetHeight;
+    const fadeStart = headerHeight / 80;
+    const fadeEnd = headerHeight;
+
+    // Adjust the fade fraction to be twice as fast
+    const fadeFraction = (scrollTop - fadeStart) / ((fadeEnd - fadeStart));
+    const videoOpacity = Math.min(Math.max(1 - fadeFraction, 0), 1);
+    
+    const video = document.querySelector('.scrolltext');
     video.style.opacity = videoOpacity;
 });
