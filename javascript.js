bee66e8eedefc2f29eb74e7ce28395cf923ce6a2
Senorpedrito <pieterwattel@gmail.com>
2024-06-19 17:04:51 +0200

    make header work on safari, delay text fade in.
    I used javascript to make sure that the video even plays in safari.
    Also I made the fadein of the "scroll down" text start later.
    finally I made the footer text slightly smaller, and removed the "ben stil met me" that was written in between there.

--- javascript.js
+++ javascript.js
@@ -30,3 +30,10 @@ window.addEventListener('scroll', () => {
     video.style.opacity = videoOpacity;
 });
 
+
+document.body.addEventListener("touchstart", function () {
+    var allVideos = document.querySelectorAll('video');
+    for (var i = 0; i < allVideos.length; i++) {
+        allVideos[i].play();
+    }
+},{ once: true });
