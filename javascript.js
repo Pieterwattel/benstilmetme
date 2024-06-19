e36c41cb452f3b70f371fef54c201d9095812eb3
Senorpedrito <pieterwattel@gmail.com>
2024-06-19 17:16:56 +0200

    made "scroll down" smaller font, removed a JS
    I removed the script that made the "scroll down" button disappear.
    It was not working anymore anyway, and I didn't find it very important

--- javascript.js
+++ javascript.js
@@ -16,20 +16,6 @@ window.addEventListener('scroll', () => {
     video.style.opacity = videoOpacity;
 });
 
-window.addEventListener('scroll', () => {
-    const scrollTop = window.scrollY;
-    const headerHeight = document.querySelector('.scrolltext').offsetHeight;
-    const fadeStart = headerHeight / 80;
-    const fadeEnd = headerHeight;
-
-    // Adjust the fade fraction to be twice as fast
-    const fadeFraction = (scrollTop - fadeStart) / ((fadeEnd - fadeStart));
-    const videoOpacity = Math.min(Math.max(1 - fadeFraction, 0), 1);
-    
-    const video = document.querySelector('.scrolltext');
-    video.style.opacity = videoOpacity;
-});
-
 
 document.body.addEventListener("touchstart", function () {
     var allVideos = document.querySelectorAll('video');
