0143e71d423d6df847a1043c7052a00b41632ed3
Pieter_W <163425907+Pieterwattel@users.noreply.github.com>
2024-10-04 12:43:45 +0200

    trying to fix video safari autoplay

--- javascript.js
+++ javascript.js
@@ -32,9 +32,17 @@ window.addEventListener('scroll', () => {
     header.style.opacity = newOpacity;
 });
 
+var allVideos = document.querySelectorAll('vid');
 document.body.addEventListener("touchstart", function () {
-    var allVideos = document.querySelectorAll('video');
+
     for (var i = 0; i < allVideos.length; i++) {
         allVideos[i].play();
     }
 },{ once: true });
+
+allVideos.play();
+
+
+let div = document.createElement("div")
+document.body.appendChild(div)
+alert(div)
