cbf9489e227d2f8ee047e2cf2fd01ab4006dd0f6
Pieter_W <163425907+Pieterwattel@users.noreply.github.com>
2024-10-28 14:34:29 +0100

    create about page

--- about/javascript.js
+++ about/javascript.js
@@ -23,18 +23,6 @@ let resizeInputOnce = true
 let giveMail = true
 
 //EVENTLISTENERS
-if (page == "index"){
-
-inputContainer.addEventListener("focusin", typeExperience)
-inputContainer.addEventListener("mouseover", typeExperience, {once: resizeInputOnce})
-
-}
-
-document.body.addEventListener("click",  function(e){
-    if (!e.target.classList.contains("experience")){
-        minimizeExperience()
-    }
-})
 buttonPiet.addEventListener("click", ()=>{
     if (giveMail){
         buttonPiet.textContent="pieterwattel@gmail.com"
@@ -69,93 +57,6 @@ links.forEach(link=>{
 body.style.backgroundcolor="blue"
 
 //FUNCTIONS
-
-
-
-    window.addEventListener('scroll', () => {
-        const scrollTop = window.scrollY;
-        const headerHeight = document.querySelector('.header').offsetHeight;
-        const fadeStart = headerHeight / 2;
-        const fadeEnd = headerHeight;
-    
-        // Adjust the fade fraction to be twice as fast
-        const fadeFraction = (scrollTop - fadeStart) / ((fadeEnd - fadeStart));
-        const videoOpacity = Math.min(Math.max(0.05 - fadeFraction, 0), 1);
-        
-        const video = document.querySelector('.header video');
-        video.style.opacity = videoOpacity;
-    });
-
-    setTimeout(() => {
-        let emptyText = document.getElementById("emptyText")
-    
-        if (window.scrollY < 100){
-                let scrollText = document.createElement("div")
-                scrollText.classList.add("scrollText")
-                scrollText.textContent=("      scroll down ↓")
-                body.insertBefore(scrollText, emptyText)
-                emptyText.remove()
-        } else {
-            return
-        }
-    }, 2000);
-    
-    function typeExperience (e){
-        if (e.target.id != 'nameInput') {
-            experienceInput.style.height="200px"
-        } else {
-        }
-    }
-    
-    function minimizeExperience(e){
-        if (minimizeExperienceFlag){
-            experienceInput.style.height=""
-            experienceInput.style.width="max(15vw + 200px, 320px)"
-        }
-    }
-
-    sendExperience.addEventListener("click", function(){
-        if (experienceInput.value != ""){
-            sendExperience.classList.add("fadeOut")
-            nameInput.classList.add("fadeOut")
-            setTimeout(() => {
-                sendExperience.remove();
-                nameInput.remove();
-            }, "1200");      
-        }
-    })
-    
-    function shareSucces(){    
-        experienceInput.value = ""
-        experienceInput.placeholder="Experience sent: \n thank you for sharing!"
-        experienceInput.style.height="100px"
-    
-    }
-    
-    //removes the extra screen when a form is sent
-    window.addEventListener("load", function() {
-        const form = document.getElementById('inputContainer');
-        form.addEventListener("submit", function(e) {
-          e.preventDefault();
-          const data = new FormData(form);
-          const action = e.target.action;
-          fetch(action, {
-            method: 'POST',
-            body: data,
-          })
-          .then(() => {
-            shareSucces()
-            minimizeExperienceFlag = false
-            console.log("minimizeExperienceFlag is false")
-          })
-        });
-    });
-    
-    
-window.onbeforeunload = function () {
-    window.scrollTo(0, 0);
-}
-
 function darkenBorder(item) {
     item.style.border="2px solid rgb(69, 69, 69)"
 }
